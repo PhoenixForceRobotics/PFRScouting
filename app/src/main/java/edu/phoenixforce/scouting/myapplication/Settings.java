b9453192f09cb40b548e68f9a4f024ef9c56d6a8
andrewbaruck <andrewbaruck@icloud.gom>
2020-01-25 13:41:09 -0800

    scouting2020 layed out

---
+++ app/src/main/java/edu/phoenixforce/scouting/myapplication/Settings.java
@@ -0,0 +1,14 @@
+package edu.phoenixforce.scouting.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
