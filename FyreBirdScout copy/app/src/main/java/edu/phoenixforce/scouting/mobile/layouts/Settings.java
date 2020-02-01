d8d6d0fdb6759c9e2a77bf591c8978d9a0a0b9e7
andrewbaruck <andrewbaruck@icloud.gom>
2020-02-01 14:24:03 -0800

    Merge

--- FyreBirdScout copy/app/src/main/java/edu/phoenixforce/scouting/mobile/layouts/Settings.java
+++ FyreBirdScout copy/app/src/main/java/edu/phoenixforce/scouting/mobile/layouts/Settings.java
@@ -2,38 +2,36 @@ package edu.phoenixforce.scouting.mobile.layouts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
+
+import android.app.DownloadManager;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 import com.example.fyrebirdscout11.R;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
-import static android.provider.Telephony.Mms.Part.FILENAME;
+
+
+
+
 
 public class Settings extends AppCompatActivity {
 
-
-    EditText deviceId;
-
     Button saver;
-    EditText pWord;
 
+    EditText passw,teamnum,devnum;
 
+    String password, teamnumber, devnumber;
 
+    TextView printout;
+    ImageView viewer;
 
 
     @Override
@@ -41,68 +39,31 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-      /*  deviceId = findViewById(R.id.deviceid);
-        saver = findViewById(R.id.save);
-        pWord = findViewById(R.id.password);*/
-
-        int correct;
-
-        String answer = pWord.getText().toString();
 
 
-        int password = Integer.parseInt(answer);
 
-        correct = 2294;
 
-        /*saver.setOnClickListener(new View.OnClickListener(){
 
-            @Override public void onClick(View v) {
-                writeFile();
+        /*teamnum = (EditText) findViewById(R.id.teamNum);
+
+        teamnum.setOnEditorActionListener(new TextView.OnEditorActionListener(){
+
+            @Override
+            public boolean onEditorActiomn(TextView v, int actionId, KeyEvent event){
+
+
+                if (actionId == EditorInfo.IME_NULL && event.getAction() == KeyEvent.ACTION_DOWN) {
+
+
+
+                }
 
             }
+
+
         });*/
 
 
 
     }
-
-   /* private void writeFile() {
-        String deviceNumber = deviceId.getText().toString();
-        try {
-           FileOutputStream fileOutputStream = openFileOutput("DeviceNum.txt", Context.MODE_PRIVATE);
-            fileOutputStream.write(deviceNumber.getBytes());
-            fileOutputStream.close();
-
-            Toast.makeText(getApplicationContext(), "text saved", Toast.LENGTH_SHORT).show();
-        }
-        catch (IOException e) {
-            Log.e("Exception", "File write failed: " + e.toString());
-        }
-    }
-
-    private void readFile() {
-
-        try {
-            FileInputStream fileInputStream = openFileInput("DeviceNum.txt");
-            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
-
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            StringBuffer stringBuffer = new StringBuffer();
-
-            String lines;
-            while ((lines = bufferedReader.readLine()) != null) {
-                stringBuffer.append(lines + "\n");
-
-            }
-
-
-            //displayText.setText(stringBuffer.toString());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-
-            e.printStackTrace();
-
-
-        }
-    }*/}
+}
