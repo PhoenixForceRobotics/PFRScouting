71f2893292fa9b1aa3aad58e732dba31b1673a6e
AndrewBaruck <43457931+AndrewBaruck@users.noreply.github.com>
2022-02-24 16:57:32 -0800

    stashing

--- app/src/main/java/edu/phoenixforce/scouting/mobile/common/Constants.java
+++ app/src/main/java/edu/phoenixforce/scouting/mobile/common/Constants.java
@@ -1,8 +1,14 @@
 package edu.phoenixforce.scouting.mobile.common;
 
-public class Constants {
+import android.util.Log;
 
+import edu.phoenixforce.scouting.*;
+import edu.phoenixforce.scouting.mobile.layouts.login;
+
+public class Constants {
     public static final int EXTERNAL_FILE_STORAGE_PERMISSION = 1;
+
+
 
 
 }
