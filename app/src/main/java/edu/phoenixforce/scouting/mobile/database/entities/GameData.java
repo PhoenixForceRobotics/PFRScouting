07eaacb985b75b3753117f23db21bd36202a2d2d
nsorlie <4007233@g.highlineschools.org>
2023-01-10 19:21:21 -0800

    "Added and changed all collumns in the entity"

--- app/src/main/java/edu/phoenixforce/scouting/mobile/database/entities/GameData.java
+++ app/src/main/java/edu/phoenixforce/scouting/mobile/database/entities/GameData.java
@@ -13,35 +13,46 @@ import org.jetbrains.annotations.NotNull;
 //2022 Game Table
 
 @Entity
-public class GameData{
+public class GameData {
 
     //Create the variable for the columns in the table
-    public GameData(@NotNull String Scout,String DevNum, String MatchNum, String TeamNum,
-                    String Moved, String Stalled, String ClimbLevel,
-                     String BallLow, String BallHigh, String AutoBallLow,
-                    String AutoBallHigh, String AutoBallCollected, String AutoBallMissed, String Collected, String Dropped, String Missed, String TeleMoved, String NoShow) {
-    //Create our getters and setters - will we ever use them tho???
+    public GameData(@NotNull String Scout, String DevNum, String MatchNum, String TeamNum,
+                    String AutoTopCones, String AutoMidCones, String AutoBottomCones, String AutoTopCubes,
+                    String AutoMidCubes, String AutoBottomCubes, String TeleTopCones, String TeleMidCones,
+                    String TeleBottomCones, String TeleTopCubes, String TeleMidCubes, String TeleBottomCubes,
+                    String Defence, String AutoMoved, String AutoLeftCommunity, String AutoOnStation,
+                    String AutoLevelOnStation, String TeleOnStation, String TeleLevelOnStation, String TeleMoved,
+                    String TeleBroke, String TeleNoShow) {
+        //Create our getters and setters - will we ever use them tho???
         //TODO THIS ^
 
 
         this.MatchNum = MatchNum;
         this.TeamNum = TeamNum;
-        this.Moved = Moved;
-        this.Stalled = Stalled;
-        this.ClimbLevel = ClimbLevel;
-        this.Collected = Collected;
-        this.Dropped = Dropped;
-        this.Missed = Missed;
-        this.BallLow = BallLow;
-        this.BallHigh = BallHigh;
-        this.AutoBallHigh = AutoBallHigh;
-        this.AutoBallLow = AutoBallLow;
         this.DevNum = DevNum;
         this.Scout = Scout;
+        this.AutoTopCones = AutoTopCones;
+        this.AutoMidCones = AutoMidCones;
+        this.AutoBottomCones = AutoBottomCones;
+        this.AutoTopCubes = AutoTopCubes;
+        this.AutoMidCubes = AutoMidCubes;
+        this.AutoBottomCubes = AutoBottomCubes;
+        this.TeleTopCones = TeleTopCones;
+        this.TeleMidCones = TeleMidCones;
+        this.TeleBottomCones = TeleBottomCones;
+        this.TeleTopCubes = TeleTopCubes;
+        this.TeleMidCubes = TeleMidCubes;
+        this.TeleBottomCubes = TeleBottomCubes;
+        this.Defence = Defence;
+        this.AutoMoved = AutoMoved;
+        this.AutoLeftCommunity = AutoLeftCommunity;
+        this.AutoOnStation = AutoOnStation;
+        this.AutoLevelOnStation = AutoLevelOnStation;
+        this.TeleOnStation = TeleOnStation;
+        this.TeleLevelOnStation = TeleLevelOnStation;
         this.TeleMoved = TeleMoved;
-        this.NoShow = NoShow;
-        this.AutoBallCollected = AutoBallCollected;
-        this.AutoBallMissed = AutoBallMissed;
+        this.TeleBroke = TeleBroke;
+        this.TeleNoShow = TeleNoShow;
     }
 
     //This is all horribly disorganized
@@ -71,61 +82,6 @@ public class GameData{
         TeamNum = teamNum;
     }
 
-    public String getMoved() {
-        return Moved;
-    }
-
-    public void setMoved(String moved) {
-        Moved = moved;
-    }
-
-    public String getStalled() {
-        return Stalled;
-    }
-
-    public void setStalled(String stalled) {
-        Stalled = stalled;
-    }
-
-    public String getClimbLevel() {
-        return ClimbLevel;
-    }
-
-
-
-
-    public String getBallLow() {
-        return BallLow;
-    }
-
-    public void setBallLow(String ballLow) {
-        BallLow = ballLow;
-    }
-
-    public String getBallHigh() {
-        return BallHigh;
-    }
-
-    public void setBallHigh(String ballHigh) {
-        BallHigh = ballHigh;
-    }
-
-    public String getAutoBallLow() {
-        return AutoBallLow;
-    }
-
-    public void setAutoBallLow(String autoBallLow) {
-        AutoBallLow = autoBallLow;
-    }
-
-    public String getAutoBallHigh() {
-        return AutoBallHigh;
-    }
-
-    public void setAutoBallHigh(String autoBallHigh) {
-        AutoBallHigh = autoBallHigh;
-    }
-
     public String getDevNum() {
         return DevNum;
     }
@@ -133,6 +89,159 @@ public class GameData{
     public void setDevNum(String devNum) {
         DevNum = devNum;
     }
+
+    public String getAutoMoved() {
+        return AutoMoved;
+    }
+
+    public void setAutoMoved(String autoMoved) {
+        AutoMoved = autoMoved;
+    }
+
+    public String getTeleMoved() { return TeleMoved; }
+
+    public void setTeleMoved(String teleMoved) { TeleMoved = teleMoved; }
+
+    public String getAutoTopCones() {
+        return AutoTopCones;
+    }
+
+    public void setAutoTopCones(String autoTopCones) {
+        AutoTopCones = autoTopCones;
+    }
+
+    public String getAutoMidCones() {
+        return AutoMidCones;
+    }
+
+    public void setAutoMidCones(String autoMidCones) {
+        AutoMidCones = autoMidCones;
+    }
+
+    public String getAutoBottomCones() {
+        return AutoBottomCones;
+    }
+
+    public void setAutoBottomCones(String autoBottomCones) { AutoBottomCones = autoBottomCones; }
+
+    public String getAutoTopCubes() {
+        return AutoTopCubes;
+    }
+
+    public void setAutoTopCubes(String autoTopCubes) {
+        AutoTopCubes = autoTopCubes;
+    }
+
+    public String getAutoMidCubes() {
+        return AutoMidCubes;
+    }
+
+    public void setAutoMidCubes(String autoMidCubes) {
+        AutoMidCubes = autoMidCubes;
+    }
+
+    public String getAutoBottomCubes() {
+        return AutoBottomCubes;
+    }
+
+    public void setAutoBottomCubes(String autoBottomCubes) {
+        AutoBottomCubes = autoBottomCubes;
+    }
+
+    public String getTeleTopCones() {
+        return TeleTopCones;
+    }
+
+    public void setTeleTopCones(String teleTopCones) {
+        TeleTopCones = teleTopCones;
+    }
+
+    public String getTeleMidCones() {
+        return TeleMidCones;
+    }
+
+    public void setTeleMidCones(String teleMidCones) {
+        TeleMidCones = teleMidCones;
+    }
+
+    public String getTeleBottomCones() {
+        return TeleBottomCones;
+    }
+
+    public void setTeleBottomCones(String teleBottomCones) { TeleBottomCones = teleBottomCones; }
+
+    public String getTeleTopCubes() {
+        return TeleTopCubes;
+    }
+
+    public void setTeleTopCubes(String teleTopCubes) {
+        TeleTopCubes = teleTopCubes;
+    }
+
+    public String getTeleMidCubes() { return TeleMidCubes; }
+
+    public void setTeleMidCubes(String teleMidCubes) {
+        TeleMidCubes = teleMidCubes;
+    }
+
+    public String getTeleBottomCubes() {
+        return TeleBottomCubes;
+    }
+
+    public void setTeleBottomCubes(String teleBottomCubes) {
+        TeleBottomCubes = teleBottomCubes;
+    }
+
+    public String getDefence() {
+        return Defence;
+    }
+
+    public void setDefence(String defence) {
+        Defence = defence;
+    }
+
+    public String getAutoLeftCommunity() {
+        return AutoLeftCommunity;
+    }
+
+    public void setAutoLeftCommunity(String autoLeftCommunity) { AutoLeftCommunity = autoLeftCommunity; }
+
+    public String getAutoOnStation() {
+        return AutoOnStation;
+    }
+
+    public void setAutoOnStation(String autoOnStation) {
+        AutoOnStation = autoOnStation;
+    }
+
+    public String getAutoLevelOnStation() {
+        return AutoLevelOnStation;
+    }
+
+    public void setAutoLevelOnStation(String autoLevelOnStation) { AutoLevelOnStation = autoLevelOnStation; }
+
+    public String getTeleOnStation() {
+        return TeleOnStation;
+    }
+
+    public void setTeleOnStation(String teleOnStation) {
+        TeleOnStation = teleOnStation;
+    }
+
+    public String getTeleLevelOnStation() {
+        return TeleLevelOnStation;
+    }
+
+    public void setTeleLevelOnStation(String teleLevelOnStation) { TeleLevelOnStation = teleLevelOnStation; }
+
+    public String getTeleBroke() {
+        return TeleBroke;
+    }
+
+    public void setTeleBroke(String teleBroke) {
+        TeleBroke = teleBroke;
+    }
+
 
     @PrimaryKey
     @NotNull
@@ -145,113 +254,72 @@ public class GameData{
     @ColumnInfo(name = "TeamNum")
     private String TeamNum;
 
-    @ColumnInfo(name = "Moved")
-    private String Moved;
-
-    @ColumnInfo(name = "Stalled")
-    private String Stalled;
-
-    @ColumnInfo(name = "ClimbLevel")
-    private String ClimbLevel;
-
-
-
-    @ColumnInfo(name = "BalLLow")
-    private String BallLow;
-
-    @ColumnInfo(name = "BallHigh")
-    private String BallHigh;
-
-    @ColumnInfo(name = "AutoBalLLow")
-    private String AutoBallLow;
-
-    @ColumnInfo(name = "AutoBallHigh")
-    private String AutoBallHigh;
-
-    @ColumnInfo(name = "AutoBallCollected")
-    private String AutoBallCollected;
-
-    @ColumnInfo(name = "AutoBallMissed")
-    private String AutoBallMissed;
-
-    @ColumnInfo(name = "DevNum")
-    private String DevNum;
-
-    public String getCollected() {
-        return Collected;
-    }
-
-    public void setCycleAttempted(String collected) {
-        Collected = collected;
-    }
-
-    @ColumnInfo(name = "Collected")
-    private String Collected;
-
-    @ColumnInfo(name = "Dropped")
-    private String Dropped;
+    @ColumnInfo(name = "AutoMoved")
+    private String AutoMoved;
 
     @ColumnInfo(name = "TeleMoved")
     private String TeleMoved;
 
-    public String getTeleMoved() {
-        return TeleMoved;
-    }
+    @ColumnInfo(name = "AutoTopCones")
+    private String AutoTopCones;
 
-    public void setTeleMoved(String teleMoved) {
-        TeleMoved = teleMoved;
-    }
+    @ColumnInfo(name = "AutoMidCones")
+    private String AutoMidCones;
 
-    public String getNoShow() {
-        return NoShow;
-    }
+    @ColumnInfo(name = "AutoBottomCones")
+    private String AutoBottomCones;
 
-    public void setNoShow(String noShow) {
-        NoShow = noShow;
-    }
+    @ColumnInfo(name = "AutoTopCubes")
+    private String AutoTopCubes;
 
-    @ColumnInfo(name = "NoShow")
-    private String NoShow;
+    @ColumnInfo(name = "AutoMidCubes")
+    private String AutoMidCubes;
 
-    public String getDropped() {
-        return Dropped;
-    }
+    @ColumnInfo(name = "AutoBottomCubes")
+    private String AutoBottomCubes;
 
+    @ColumnInfo(name = "TeleTopCones")
+    private String TeleTopCones;
 
+    @ColumnInfo(name = "TeleMidCones")
+    private String TeleMidCones;
 
-    public String getMissed() {
-        return Missed;
-    }
+    @ColumnInfo(name = "TeleBottomCones")
+    private String TeleBottomCones;
 
-    public void setMissed(String missed) {
-        Missed = missed;
-    }
+    @ColumnInfo(name = "TeleTopCubes")
+    private String TeleTopCubes;
 
-    @ColumnInfo(name = "Missed")
-    private String Missed;
+    @ColumnInfo(name = "TeleMidCubes")
+    private String TeleMidCubes;
 
+    @ColumnInfo(name = "TeleBottomCubes")
+    private String TeleBottomCubes;
 
-    public void setClimbLevel(String climbLevel) {
-        ClimbLevel = climbLevel;
-    }
+    @ColumnInfo(name = "Defence")
+    private String Defence;
 
-    public String getAutoBallCollected() {
-        return AutoBallCollected;
-    }
+    @ColumnInfo(name = "AutoLeftCommunity")
+    private String AutoLeftCommunity;
 
-    public void setAutoBallCollected(String autoBallCollected) {
-        AutoBallCollected = autoBallCollected;
-    }
+    @ColumnInfo(name = "AutoLevelOnStation")
+    private String AutoLevelOnStation;
 
-    public String getAutoBallMissed() {
-        return AutoBallMissed;
-    }
+    @ColumnInfo(name = "TeleOnStation")
+    private String TeleOnStation;
 
-    public void setAutoBallMissed(String autoBallMissed) {
-        AutoBallMissed = autoBallMissed;
-    }
+    @ColumnInfo(name = "TeleLevelOnStation")
+    private String TeleLevelOnStation;
 
-    public void setDropped(String dropped) {
-        Dropped = dropped;
-    }
+    @ColumnInfo(name = "AutoOnStation")
+    private String AutoOnStation;
+
+    @ColumnInfo(name = "TeleBroke")
+    private String TeleBroke;
+
+    @ColumnInfo(name = "DevNum")
+    private String DevNum;
+
+    @ColumnInfo(name = "TeleNoShow")
+    private String TeleNoShow;
 }
